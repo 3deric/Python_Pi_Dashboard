44f9a424cc1c9a4abe0b44afb3e5a198656f4e52
Eric <eric.schubert.3d@gmail.com>
2026-02-05 14:14:56 +0100

    User interface improvements

--- user_interface.py
+++ user_interface.py
@@ -14,33 +14,42 @@ def page(client : Client):
     }
     ''')
 
-    with ui.grid(columns= 2).classes('w-[800px] h-[480px] bg-green gap-0 p-0').style('grid-template-columns: auto auto auto auto'):
+    with ui.grid(columns = 2, rows = 1).classes('w-[800px] h-[480px] bg-grey gap-2 p-2').style('grid-template-columns: 1fr 2fr'):
 
-        with ui.column().classes(f'w-[300px] h-full relative gap-1'):
-            ui.label('Time').classes('absolute top-1 right-1 font-bold')
+        with ui.column().classes('w-full h-full relative gap-2 p-0'):
+            with ui.card().classes('w-full h-50 gap-2 p-2'):
+                with ui.column(wrap = False, align_items = 'center').classes('w-full h-full gap-2 p-2'):
+                    ui.label(datetime.now().strftime('%H:%M')).style('font-size: 600%; font-weight: 600; line-height: 1')
+                    ui.label(datetime.now().strftime('%A, %D')).style('font-size: 150%; font-weight: 300; line-height: 0.5')
+            with ui.card().classes('w-full h-full gap-0 p-0'):
+                ui.label('Weather').classes('font-bold')
 
-        with ui.column().classes(f'w-[500px] h-full relative gap-1'):
+        with ui.column().classes('w-full h-full relative gap-0 p-0'):
             # right panel
-            with ui.tabs().classes('w-full') as tabs:
-                ui.tab('tab_transport', label = '', icon = 'o_directions_bus')
-                ui.tab('tab_weather', label = '', icon = 'o_cloud')
-                ui.tab('tab_calendar', label = '', icon = 'o_calendar_month')
-            with ui.tab_panels(tabs, value = 'tab_transport').classes('w-full h-full'):
-                with ui.tab_panel('tab_transport').classes('w-full h-full gap-1 p-1'):
-                    for i in range(7):
-                        with ui.card().classes(f'w-full h-full'):
-                            ui.label('Line and Dir').classes('absolute top-1 left-2 font-bold')
-                            # Top-right label
-                            ui.label('Delta Time').classes('absolute top-1 right-2 font-bold')
-                            # Bottom-left label
-                            ui.label('State').classes('absolute bottom-1 left-2 font-regular')
-                            # Bottom-right label
-                            ui.label('Real Time').classes('absolute bottom-1 right-2 font-regular')
-                with ui.tab_panel('tab_weather').classes('w-full h-full gap-0'):
-                    pass
+            with ui.card().classes('w-full h-full gap-0 p-0'):
+                with ui.tabs().classes('w-full') as tabs:
+                    ui.tab('tab_transport', label = '', icon = 'o_directions_bus')
+                    ui.tab('tab_weather', label = '', icon = 'o_cloud')
+                    ui.tab('tab_calendar', label = '', icon = 'o_calendar_month')
+                with ui.tab_panels(tabs, value = 'tab_transport').classes('w-full h-full'):
+                    #tab panel transport
+                    with ui.tab_panel('tab_transport').classes('gap-0 p-0 w-full h-full gap-0 p-0'):
+                        with ui.list().props('dense separator').classes('w-full h-full'):
+                            for i in range(10):
+                                with ui.item():
+                                    with ui.item_section().props('middle_panel'):
+                                        ui.label('Line and Dir').classes('font-bold')
+                                        ui.label('State').classes('font-regular')
+                                    with ui.item_section().props('right_panel'):
+                                        ui.label('Delta Time').classes('text-right font-bold')
+                                        ui.label('Real Time').classes('text-right font-regular')
+                    #tab panel weather
+                    with ui.tab_panel('tab_weather').classes('w-full h-full gap-0'):
+                        pass
 
-                with ui.tab_panel('tab_calendar').classes('w-full h-full gap-0 p-0'):
-                    pass
+                    #tab panel calendar
+                    with ui.tab_panel('tab_calendar').classes('w-full h-full gap-0 p-0'):
+                        pass
 
 
 if __name__ in {"__main__", "__mp_main__"}:
